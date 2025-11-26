dda6961753588cbbb8b07f1888f360603fe83654
Sagar <sagarsstha101@gmail.com>
2025-11-26 20:01:55 +0545

    Day 17 Completed

---
+++ Day 17-Movie App/script.js
@@ -0,0 +1,26 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=72823d0fa99dedc7a2e02240ca7dd597&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=72823d0fa99dedc7a2e02240ca7dd597&query="';
+
+const form = document.getElementById('form')
+// get initial movies
+getMovies(API_URL)
+
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    
+}
+
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value;
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm);
+        search.value = '';
+    } else {
+        window.location.reload()
+    }
+ })
